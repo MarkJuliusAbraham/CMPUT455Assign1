2e769af831a30b0307f7c2e914c3e72b8d427ad0
Mark Julius Abraham <hanatodev@gmail.com>
2024-09-12 01:08:17 -0600

    finished 2 functions

--- assignment1/a1.py
+++ assignment1/a1.py
@@ -8,6 +8,8 @@ class CommandInterface:
     # The following is already defined and does not need modification
     # However, you may change or add to this code as you see fit, e.g. adding class variables to init
 
+    
+
     def __init__(self):
         # Define the string to function command mapping
         self.command_dict = {
@@ -19,6 +21,11 @@ class CommandInterface:
             "genmove" : self.genmove,
             "winner" : self.winner
         }
+
+        self.board = None
+        self.x = None
+        self.y = None
+        self.current_player = None
 
     # Convert a raw string to a command and a list of arguments
     def process_command(self, str):
@@ -64,15 +71,91 @@ class CommandInterface:
     #======================================================================================
 
     def game(self, args):
-        raise NotImplementedError("This command is not yet implemented.")
+
+        if len(args) != 2 or not args[0].isdigit() or not args[1].isdigit():
+            print("Invalid arguments for game")
+            return False
+        self.x = int(args[0])
+        self.y = int(args[1])
+
+        if not (1 <= self.x <= 20) or not (1 <= self.y <= 20):
+            print("Invalid dimensions: n and m must be between 1 and 20")
+            return False
+
+        self.board = [['.' for _ in range(self.x)] for _ in range(self.y)]
+        self.current_player = 1  # Start with player 1
         return True
-    
+                
     def show(self, args):
-        raise NotImplementedError("This command is not yet implemented.")
+
+        if(self.board == None):
+            print("game is not initialized")
+            return False
+
+            #if necessary to create a default state for the game
+            #self.game(['3','3']) 
+        
+        for row in self.board:
+            print(''.join(row))
+            
         return True
     
     def play(self, args):
-        raise NotImplementedError("This command is not yet implemented.")
+
+        if len(args) != 3:
+            print("Illegal move: " + " ".join(args) + " wrong number of arguments")
+            return False
+        
+        if ( not args[0].isdigit() or not args[1].isdigit() or not args[2].isdigit()):
+            print("Illegal move: " + " ".join(args) + " are not digits")
+            return False
+
+        x = int(args[0])
+        y = int(args[1])
+        digit = int(args[2])
+
+        if(digit != 0 and digit != 1):
+            print("digit must be either 0 or 1")
+            return False
+        
+        if( not(0 <= x <= self.x-1) or not(0 <= y <= self.y-1)):
+            print("Position is out of bounds")
+            return False
+
+        if(self.board[y][x] != '.'):
+            print("illegal move: " + " ".join(args) + " places on a taken spot")
+            return False
+        
+        #missing neighbour testing
+            # check if the position is greater than 2 for the x and y to avoid checking for the 2 left neighbors or 2 upper neighbors
+                # else: ( 1 neighbor or no neighbor to the left)
+            # check if the position is lower than x-3
+
+        
+
+        sum = digit
+        for step in range(3):
+
+            for offset in range(3):
+                if(self.board[y][x-2+offset].isdigit()):
+                    print(x-2+offset)
+                    sum += int(self.board[y][x-2+offset])
+            
+            print(sum)
+            if not (0 < sum < 3):
+                print("Invalid")
+            sum = digit
+
+
+            
+
+
+        #missing balancing case
+
+        #reaching this part assumes the above cases causes a legal move
+        #x and y are inverted due to the nesting of the arrays
+        self.board[y][x] = str(digit)
+        
         return True
     
     def legal(self, args):
