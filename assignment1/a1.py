75261b0c3480ea5166700f79cf1458b89e649f3b
Mark Julius Abraham <hanatodev@gmail.com>
2024-09-13 23:52:17 -0600

    changed to 1d array and reimplemented game, command, and play functions

--- assignment1/a1.py
+++ assignment1/a1.py
@@ -23,8 +23,8 @@ class CommandInterface:
         }
 
         self.board = None
-        self.x = None
-        self.y = None
+        self.max_x = None
+        self.max_y = None
         self.current_player = None
 
     # Convert a raw string to a command and a list of arguments
@@ -75,14 +75,28 @@ class CommandInterface:
         if len(args) != 2 or not args[0].isdigit() or not args[1].isdigit():
             print("Invalid arguments for game")
             return False
-        self.x = int(args[0])
-        self.y = int(args[1])
+        self.max_x = int(args[0])
+        self.max_y = int(args[1])
 
-        if not (1 <= self.x <= 20) or not (1 <= self.y <= 20):
+        if not (1 <= self.max_x <= 20) or not (1 <= self.max_y <= 20):
             print("Invalid dimensions: n and m must be between 1 and 20")
             return False
 
-        self.board = [['.' for _ in range(self.x)] for _ in range(self.y)]
+        # 2d array
+        # self.board = [['.' for _ in range(self.x)] for _ in range(self.y)]
+
+        self.board = []
+        for _ in range(self.max_x+1):
+            self.board.append("#")
+
+        for _ in range(self.max_y):
+            self.board.append('#')
+            for _ in range(self.max_x):
+                self.board.append('.')
+
+        for _ in range(self.max_x+1):
+            self.board.append("#")
+
         self.current_player = 1  # Start with player 1
         return True
                 
@@ -94,10 +108,17 @@ class CommandInterface:
 
             #if necessary to create a default state for the game
             #self.game(['3','3']) 
-        
-        for row in self.board:
-            print(''.join(row))
+
+        pos = 0
+        for item in self.board:
             
+            if item != "#":
+                print(item,end="")
+            if (pos%( self.max_x+1)) == 0:
+                print("\n",end="")
+            pos += 1
+        
+        print('\n')
         return True
     
     def play(self, args):
@@ -112,50 +133,33 @@ class CommandInterface:
 
         x = int(args[0])
         y = int(args[1])
-        digit = int(args[2])
+        digit = args[2]
 
-        if(digit != 0 and digit != 1):
-            print("digit must be either 0 or 1")
+        if(digit != '0' and digit != '1'):
+            print("digit must be either '0' or '1'")
             return False
         
-        if( not(0 <= x <= self.x-1) or not(0 <= y <= self.y-1)):
+        if( not(0 <= x <= self.max_x-1) or not(0 <= y <= self.max_y-1)):
             print("Position is out of bounds")
             return False
 
-        if(self.board[y][x] != '.'):
+        pos_in_1d_array = (self.max_x+1)*(y+1)+(x+1)
+
+        if(self.board[pos_in_1d_array] != '.'):
             print("illegal move: " + " ".join(args) + " places on a taken spot")
             return False
         
-        #missing neighbour testing
-            # check if the position is greater than 2 for the x and y to avoid checking for the 2 left neighbors or 2 upper neighbors
-                # else: ( 1 neighbor or no neighbor to the left)
-            # check if the position is lower than x-3
-
-        
-
-        sum = digit
-        for step in range(3):
-
-            for offset in range(3):
-                if(self.board[y][x-2+offset].isdigit()):
-                    print(x-2+offset)
-                    sum += int(self.board[y][x-2+offset])
-            
-            print(sum)
-            if not (0 < sum < 3):
-                print("Invalid")
-            sum = digit
-
-
-            
-
+        #neighbour testing
+        if not (self.neighbour_test(x,y,digit)):
+            print("Creates a 3-of-the-same block")
+            return False
 
         #missing balancing case
 
         #reaching this part assumes the above cases causes a legal move
-        #x and y are inverted due to the nesting of the arrays
-        self.board[y][x] = str(digit)
-        
+
+        #math to place in a 1d array with borders
+        self.board[pos_in_1d_array] = digit
         return True
     
     def legal(self, args):
@@ -173,6 +177,104 @@ class CommandInterface:
     #======================================================================================
     # End of functions requiring implementation
     #======================================================================================
+
+    
+    def neighbour_test(self, x, y, digit):
+        """
+            Args: 
+                x: x position relative to the board 
+                y: y position relative to the board
+                digit: the digit to be placed
+            Returns:
+                bool: True if the digit has no similar neighbors that creates a 3-of-the-same block. False otherwise.
+        """
+
+        # x_sum = 0
+        # y_sum = 0
+        # for step in range(3):
+        #     for offset in range(3):
+        #         x_pos = x + step + offset - 2
+        #         y_pos = y + step + offset - 2
+        #         if(0 <= x_pos < self.x):
+
+        #             if(self.board[y][x_pos].isdigit()):
+        #                 x_sum += int(self.board[y][x_pos])==digit
+
+        #         if(0 <= y_pos < self.y):
+
+        #             if(self.board[y_pos][x].isdigit()):
+        #                 y_sum += int(self.board[y_pos][x])==digit
+
+        #     if (x_sum == 2):
+        #         return False
+        #     elif (y_sum == 2):
+        #         return False
+        #     x_sum = 0    
+        #     y_sum = 0
+
+
+        #y_offset_var traverses the column per y increase
+        y_offset_var = self.y_offset_var(y,0)
+        #x_offset_var traverses the row per given x
+        x_offset_var = (x+1)
+
+        pos_in_1d_array = y_offset_var+x_offset_var
+        
+        #do row check
+
+        #check if left is the same as digit
+        if(self.board[pos_in_1d_array-1] == digit):
+            #check if right is also the same OR the twice-left is the same
+            if(self.board[pos_in_1d_array+1]==digit or self.board[pos_in_1d_array-2]==digit):
+                return False
+        elif(self.board[pos_in_1d_array+1]==digit and self.board[pos_in_1d_array+2]==digit):
+            return False
+
+    
+        #do column check
+
+
+        # self.board[max(0, min(pos_in_1d_array - 2 * y_offset_var, len(self.board) - 1))]
+
+
+        print(pos_in_1d_array)
+        print(pos_in_1d_array + 1 * y_offset_var)
+        print(pos_in_1d_array + self.y_offset_var(y,1))
+        print(len(self.board))
+        
+
+        clamped_value = None
+        if(self.board[pos_in_1d_array+self.y_offset_var(y,-1)] == digit):
+            #check if right is also the same OR the twice-left is the same
+            if(self.board[pos_in_1d_array+self.y_offset_var(y,1)] == digit):
+                print("case 1")
+                return False
+
+            clamped_value = pos_in_1d_array+self.y_offset_var(y,-2)
+            if(clamped_value < 0):
+                clamped_value = 0
+            
+            if(self.board[clamped_value]==digit):
+                print("case 2")
+                return False
+        elif(self.board[pos_in_1d_array + self.y_offset_var(y,1)] == digit):
+            clamped_value = pos_in_1d_array + self.y_offset_var(y,2)
+            if(clamped_value > len(self.board)-1):
+                clamped_value = len(self.board)-1
+
+            if(self.board[clamped_value] == digit):
+                print("case 3")
+                return False
+        
+        return True
+    
+    def y_offset_var(self, y, shift):
+        """
+            Returns:
+                The Y offset increase in the 1d array per increment or decrement of the y-positional value
+        """
+        return (self.max_x+1)*(shift)
+
 
 if __name__ == "__main__":
     interface = CommandInterface()
---
+++ matrix.py
@@ -0,0 +1,11 @@
+
+
+b =[['.','1','1','.'],['.','.','.','.'],['.','.','.','.']]
+
+
+def show(item):
+    for row in item:
+            print(''.join(row))
+
+
+show(b)
